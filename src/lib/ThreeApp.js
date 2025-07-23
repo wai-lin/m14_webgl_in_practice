b83ed5f76502a4ccf2bdcfb9f147389782ad4229
wai-lin <dev.wailin@gmail.com>
2025-07-23 11:49:45 +0700

    feat: make debugging tools toggable

--- src/lib/ThreeApp.js
+++ src/lib/ThreeApp.js
@@ -5,12 +5,8 @@ import { OrbitControls } from "three/addons/controls/OrbitControls";
 export class THREEApp {
   // CONSTANTS
   #MAX_PIXEL_RATIO = 1.6;
+  #DEBUG = false;
 
-  /**
-   * Stats.js instance for performance monitoring
-   * @type {Stats}
-   */
-  #stats;
   /**
    * Three.js WebGL Renderer
    * @type {THREE.WebGLRenderer}
@@ -26,11 +22,6 @@ export class THREEApp {
    * @type {THREE.PerspectiveCamera}
    */
   #camera;
-  /**
-   * Orbit Controls for the camera
-   * @type {OrbitControls}
-   */
-  #controls;
   /**
    * Application current scene
    * @type {THREE.Scene}
@@ -48,6 +39,18 @@ export class THREEApp {
    */
   #onAnimate;
 
+  // DEBUGGING TOOLS
+  /**
+   * Stats.js instance for performance monitoring
+   * @type {Stats}
+   */
+  #stats;
+  /**
+   * Orbit Controls for the camera
+   * @type {OrbitControls | undefined}
+   */
+  #controls;
+
   /**
    * Creates an instance of the ThreeJsApp class.
    * @param {string} canvasId
@@ -62,6 +65,35 @@ export class THREEApp {
     this.#init();
     this.#initEvents();
   }
+
+  // == DEBUGGING TOOLS ==
+
+  get DEBUG() {
+    return this.#DEBUG;
+  }
+  set DEBUG(value) {
+    this.#DEBUG = value;
+    if (this.#DEBUG) {
+      this.#setDebugers();
+    } else {
+      this.#stats?.end();
+      this.#stats = undefined;
+
+      this.#controls?.dispose();
+      this.#controls = undefined;
+    }
+  }
+
+  #setDebugers() {
+    // STATS
+    this.#stats = new Stats();
+    document.body.appendChild(this.#stats.dom);
+
+    // CONTROLS
+    this.#controls = new OrbitControls(this.#camera, this.#gl.domElement);
+  }
+
+  // == PRIVATE METHODS ==
 
   #getContext() {
     /** @type {THREEApp.AppContext} */
@@ -87,18 +119,14 @@ export class THREEApp {
     // Set the size of the renderer to match the window size
     this.#gl.setSize(window.innerWidth, window.innerHeight);
 
-    // STATS
-    this.#stats = new Stats();
-    document.body.appendChild(this.#stats.dom);
-
     // CAMERA
     const aspectRatio = window.innerWidth / window.innerHeight;
     this.#camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 1000);
     this.#camera.position.set(0, 0, 5);
     this.#camera.lookAt(0, 0, 0);
 
-    // CONTROLS
-    this.#controls = new OrbitControls(this.#camera, this.#gl.domElement);
+    // ENABLE DEBUGGING TOOLS
+    if (this.DEBUG) this.#setDebugers();
   }
 
   #initEvents() {
@@ -114,7 +142,7 @@ export class THREEApp {
   }
 
   #animate() {
-    this.#stats.begin();
+    this.#stats?.begin();
 
     this.#gl.render(this.#scene, this.#camera);
 
@@ -124,7 +152,7 @@ export class THREEApp {
         setScene: this.setScene.bind(this),
       });
 
-    this.#stats.end();
+    this.#stats?.end();
 
     window.requestAnimationFrame(this.#animate.bind(this));
   }
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { THREEApp } from "./lib/ThreeApp";
 import { animateCubicScene, createCubicScene } from "./scenes/cubic.scene";
 
 const app = new THREEApp("#webgl-canvas");
+app.DEBUG = true; // Enable debug mode for development
 app.setScene(createCubicScene);
 app.onAnimate(animateCubicScene);
 app.start();
