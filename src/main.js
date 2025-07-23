4be13101a3170c8ac46e5bf3a04e5dd2f7ae3ae8
wai-lin <dev.wailin@gmail.com>
2025-07-23 10:47:51 +0700

    feat: impl test application

--- src/main.js
+++ src/main.js
@@ -1,1 +1,28 @@
-import './style.css'
+import "./style.css";
+import * as THREE from "three";
+import { THREEApp } from "./ThreeApp";
+
+const app = new THREEApp("#webgl-canvas");
+
+app.setScene(({ sceneObjects }) => {
+  const scene = new THREE.Scene();
+
+  const geo = new THREE.BoxGeometry(1, 1, 1);
+  const mat = new THREE.MeshBasicMaterial();
+  const mesh = new THREE.Mesh(geo, mat);
+  sceneObjects.set("cube", mesh);
+  scene.add(sceneObjects.get("cube"));
+
+  return scene;
+});
+
+app.onAnimate(({ sceneObjects }) => {
+  /** @type {THREE.Mesh} */
+  const cube = sceneObjects.get("cube");
+  if (cube) {
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+  }
+});
+
+app.start();
