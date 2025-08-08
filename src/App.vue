38136215219cc598d664bea4ac534c5460b218e6
wai-lin <dev.wailin@gmail.com>
2025-08-08 16:30:15 +0700

    feat: setup audio visualiser scene

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,33 @@
 <script setup lang="ts">
 import AudioPlayer from "@components/AudioVisualiser/AudioPlayer.vue";
 import WebGL from "@components/WebGL.vue";
+import {
+	OrbitControlsPlugin,
+	StatsPlugin,
+	WindowResizePlugin,
+} from "@lib/Plugins";
 import { AUDIO_VIS_POSTPROC } from "@post_processors/audio_vis.postproc";
 import { AUDIO_VIS_SCENE } from "@scenes/audio_vis.scene";
 
 const modules: ProgramTypes.Module[] = [
-	// CAR_KIT_SCENE,
-	// CAR_KIT_POSTPROC,
-	// GRID_SHADER_CAMERA,
-	// GRID_SHADER_SCENE,
+	/** Debuggers **/
+	StatsPlugin,
+	OrbitControlsPlugin,
+	// AxesHelperPlugin,
+	// CameraHelperPlugin,
+
+	/** Helpers **/
+	WindowResizePlugin,
+
+	/** Scenes **/
 	AUDIO_VIS_SCENE,
+
+	/** Post-Processing **/
 	AUDIO_VIS_POSTPROC,
 ];
 </script>
 
 <template>
-	<WebGL :modules="modules">
-		<!-- <CarKitUI /> -->
-	</WebGL>
+	<WebGL :modules="modules" />
 	<AudioPlayer class="fixed bottom-8 left-8" />
 </template>
